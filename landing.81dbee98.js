03e0b6c3ec5e0636ab9e410ee6a6d91f70a85790
Wu, Tai-Cheng <210114@gs.hs.ntnu.edu.tw>
2025-04-11 22:29:53 +0800

    Updates

---
+++ landing.81dbee98.js
@@ -0,0 +1,2 @@
+window.onbeforeunload=function(e){(leftoutelement=document.getElementsByClassName("out")).forEach(function(e,o){e.style.animation="slide-out-blurred-left 0.5s forwards"})};
+//# sourceMappingURL=landing.81dbee98.js.map
---
+++ landing.ea41651d.js
@@ -0,0 +1,8 @@
+window.onbeforeunload = function(e) {
+    leftoutelement = document.getElementsByClassName("out");
+    leftoutelement.forEach(function(item, index) {
+        item.style.animation = "slide-out-blurred-left 0.5s forwards";
+    });
+};
+
+//# sourceMappingURL=landing.ea41651d.js.map
